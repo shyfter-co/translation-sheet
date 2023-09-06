13161cec35019280d3b787ce1ae60080060132d6
JolitaG <jolita@shyfter.co>
2023-09-06 10:08:32 +0200

    add laravel notifications dependency, change push repositories command return

--- src/Commands/PushToRepositories.php
+++ src/Commands/PushToRepositories.php
@@ -2,12 +2,11 @@
 
 namespace Nikaia\TranslationSheet\Commands;
 
-use Nikaia\TranslationSheet\Notifications\TranslationsPushedNotification;
 use Illuminate\Console\Command;
 use Nikaia\TranslationSheet\SheetPusher;
 use Nikaia\TranslationSheet\Spreadsheet;
 use Illuminate\Support\Facades\Notification;
-use Spatie\SlackAlerts\Facades\SlackAlert;
+use Nikaia\TranslationSheet\Notifications\TranslationsPushedNotification;
 
 class PushToRepositories extends Command
 {
@@ -22,7 +21,7 @@ class PushToRepositories extends Command
         $repositories = config('translation_sheet.extra_sheets');
         $basePath = config('translation_sheet.base_path');
 
-        collect($repositories)->each(function ($repo) use ($basePath) {
+        $processedRepositories = collect($repositories)->map(function ($repo) use ($basePath) {
             $repository = $repo['repo'];
             $this->info("Preparing to push to git repository: $repository");
             $name = $repo['name'];
@@ -36,29 +35,39 @@ class PushToRepositories extends Command
             if (empty($output)) {
                 $this->info("No changes found on branch: $branch");
                 $this->info("Deleting branch: $branch");
-                exec("cd $directory && git checkout $master && git branch -D $branch");
-                $message = "No changes found on branch: $branch. Branch: $branch deleted.";
-                SlackAlert::to('translations')->message($message);
+                exec("cd $directory
+                                    && git checkout $master
+                                    && git branch -D $branch",
+                    $output,
+                    $result
+                );
             } else {
                 $this->info("Preparing to push to git repository: $repository");
-                exec("cd $directory && git checkout $branch && git add . && git commit -m 'updating translation' && git push origin $branch", $output, $result);
-
-                switch ($result) {
-                    case Command::SUCCESS:
-                        $message = "Git pushed branch $branch to the repository $repository";
-                        $this->info($message);
-                        SlackAlert::to('translations')->message($message);
-                    case Command::FAILURE:
-                        $message = "Could not push branch $branch to the repository $repository";
-                        $this->error("Repository [$repository] could not get cloned!");
-                        SlackAlert::to('translations')->message($message);
-                }
-
-                Notification::route('mail', 'jg@shyfter.co')
-                    ->route('mail', 'lh@shyfter.co')
-                    ->route('mail', 'mk@shyfter.co')
-                    ->notify(new TranslationsPushedNotification($repository, $branch));
+                exec("cd $directory && git checkout $branch
+                                            && git add .
+                                            && git commit -m 'updating translation'
+                                            && git push origin $branch",
+                    $output,
+                    $result
+                );
             }
+            return [$output, $result, $branch, $repository];
         });
+
+//        switch ($result) {
+//            case Command::SUCCESS:
+//                return  "Git pushed branch $branch to the repository $repository";
+//            case Command::FAILURE:
+//                return "Could not push branch $branch to the repository $repository";
+//        }
+
+        // Notifications
+//        $processedRepositories
+
+//        Notification::route('mail', 'jg@shyfter.co')
+//            ->route('mail', 'lh@shyfter.co')
+//            ->route('mail', 'mk@shyfter.co')
+//            ->notify(new TranslationsPushedNotification($repository, $branch))
+        ;
     }
 }
